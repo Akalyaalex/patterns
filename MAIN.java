53e20809f823ac683ec160babb0fe7757394f30d
Akalyaalex <114275126+Akalyaalex@users.noreply.github.com>
2023-03-14 10:19:33 +0530

    Add files via upload

---
+++ MAIN.java
@@ -0,0 +1,17 @@
+public class MAIN {
+    public static void main(String[] args){
+        int size = 5;
+        for(int i = 1; i<=size; i++){
+            for(int j=0;j<i;j++){
+            System.out.println("*");
+        }
+        System.out.println();
+    }
+    for(int i=1; i<=size-1;i++){
+        for(int j=0;j<=size-i;j++){
+            System.out.println("*");
+        }
+        System.out.println();
+    }
+}
+}
